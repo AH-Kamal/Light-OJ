3e8dbfd4cb597d6cbab8cbfa54b51ef282d2ebd0
AH-Kamal <37199027+AH-Kamal@users.noreply.github.com>
2020-01-19 11:14:16 +0600

    Add files via upload

---
+++ 1305 - Area of a Parallelogram.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long
+
+int main(){
+    int T; cin>>T;
+    for(int t = 1; t <= T; t++){
+        LL ax, ay, bx, by, cx, cy; cin>>ax>>ay>>bx>>by>>cx>>cy;
+        LL len = abs(ax-bx), width = abs(ay-by);/// length of lines
+        LL dx, dy;
+        LL area = abs(ax*(by-cy) + bx*(cy-ay) + cx*(ay-by));
+        if(bx > ax) dx = cx - len;
+        else dx = cx + len;
+        if(by > ay) dy = cy - width;
+        else dy = cy + width;
+        printf("Case %d: %d %d %d\n", t, dx, dy, area);
+    }
+
+}
